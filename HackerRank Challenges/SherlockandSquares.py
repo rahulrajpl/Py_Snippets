780fa96e5df2ee1217f5a7d3461ec98df42d90ae
rahulrajpl <lukrahul@gmail.com>
2018-06-27 07:12:10 +0530

    O(1) method using Math

--- HackerRank Challenges/SherlockandSquares.py
+++ HackerRank Challenges/SherlockandSquares.py
@@ -8,15 +8,15 @@ import sys
 
 # Complete the squares function below.
 def squares(a, b):
-    count = 0
-    p = math.sqrt(a)
-    if p % 1 == 0: count += 1
-    while True:
-        p = int(p) + 1
-        i = pow(int(p), 2)
-        if i > b: return count
-        count += 1
-
+    # count = 0
+    # p = math.sqrt(a)
+    # if p % 1 == 0: count += 1
+    # while True:
+    #     p = int(p) + 1
+    #     i = pow(int(p), 2)
+    #     if i > b: return count
+    #     count += 1
+    return math.floor(math.sqrt(b))-math.ceil(math.sqrt(a)) + 1
 
 
 if __name__ == '__main__':
