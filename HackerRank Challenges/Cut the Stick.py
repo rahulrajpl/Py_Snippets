ebcaebb19e6e87b2f2692cd16b438d5621c9c519
rahulrajpl <lukrahul@gmail.com>
2018-06-27 07:29:03 +0530

    Usage of Map, Filter and Lambda for evaluate problem

---
+++ HackerRank Challenges/Cut the Stick.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def cutTheSticks(arr):
+    cuts = []
+    cut_length = min(arr)
+    while len(arr):
+        cut_length = min(arr)
+        cuts.append(len(arr))
+        arr = list(map(lambda x:x-cut_length, arr))
+        arr = list(filter(lambda j:j!=0, arr))
+    return cuts
+
+if __name__ == '__main__':
+    n = 8
+
+    arr = list(map(int, "1 2 3 4 3 3 2 1".rstrip().split()))
+
+    result = cutTheSticks(arr)
+    print(result)
